70b3e5e5ace57c2ce4409fcc05227b8b9372a0d6
shrutis <shruti.s.2702@gmail.com>
2025-04-16 21:23:31 +0530

    added validation for string

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,12 +4,20 @@ class StringCalculator
 		return 0 if str.empty?
 
 		sum = 0 
+		is_valid = validate(str)
+		return "Invalid Input" if !is_valid
 		nums = str.scan(/\d+/)
 		nums.each do |s|
 			sum += s.to_i
 		end
 
 		sum		
+	end
+
+
+	def self.validate str
+		regex = /\A( ?\d+ ?([\n,] ?\d+)*)*\z/
+		return !!(str =~ regex)
 	end
 
 end
