5c8ca4566bb42aa3fb7ff2a761d32ffc1e28b013
shrutis <shruti.s.2702@gmail.com>
2025-04-16 19:29:19 +0530

    fix for test cases on empty string and comma separated number string

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,15 @@ class String
 
 	## method name begins with _ to avoid chances of name conflicts with existing method
 	def _add
-		
+		return 0 if self.empty?
+
+		sum = 0 
+		nums = self.scan(/\d+/)
+		nums.each do |s|
+			sum += s.to_i
+		end
+
+		sum		
 	end
 
 end
