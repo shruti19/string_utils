149971e48d5ca89b4d4887172268559ac35bd61f
shrutis <shruti.s.2702@gmail.com>
2025-04-16 19:36:13 +0530

    refactored class and method signature

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,10 @@
-class String
+class StringCalculator
 
-	## method name begins with _ to avoid chances of name conflicts with existing method
-	def _add
-		return 0 if self.empty?
+	def self.add str
+		return 0 if str.empty?
 
 		sum = 0 
-		nums = self.scan(/\d+/)
+		nums = str.scan(/\d+/)
 		nums.each do |s|
 			sum += s.to_i
 		end
--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -3,20 +3,20 @@ require_relative '../string_calculator.rb'
 
 class TestStringCalculator < Minitest::Test
 
-  describe "StringCalculator#_add" do
+  describe "StringCalculator#add" do
   	
-    it "should have an _add method" do
-      assert_equal ''.respond_to?(:_add), true
+    it "should have an add method" do
+      assert_equal StringCalculator.respond_to?(:add), true
   	end
 
     it "should return sum as 0 if called on empty string" do
-      assert_equal ''._add, 0 
+      assert_equal StringCalculator.add(''), 0 
     end
 
     it "should return sum of integers present in string separated by comma" do
-      assert_equal '1'._add, 1
-      assert_equal '1,2'._add, 3
-      assert_equal '0,3,2,5'._add, 10
+      assert_equal StringCalculator.add('1'), 1
+      assert_equal StringCalculator.add('1,2'), 3
+      assert_equal StringCalculator.add('0,3,2,5'), 10
     end
 
   end
