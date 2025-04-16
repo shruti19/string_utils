f9c360e2552605b20b997dc78ecdae1a41b71649
shrutis <shruti.s.2702@gmail.com>
2025-04-16 19:14:12 +0530

    minitests for empty string and comma separated number string

--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -3,11 +3,21 @@ require_relative '../string_calculator.rb'
 
 class TestStringCalculator < Minitest::Test
 
-  describe "StringCalculator" do
+  describe "StringCalculator#_add" do
   	
     it "should have an _add method" do
       assert_equal ''.respond_to?(:_add), true
   	end
+
+    it "should return sum as 0 if called on empty string" do
+      assert_equal ''._add, 0 
+    end
+
+    it "should return sum of integers present in string separated by comma" do
+      assert_equal '1'._add, 1
+      assert_equal '1,2'._add, 3
+      assert_equal '0,3,2,5'._add, 10
+    end
 
   end
 
