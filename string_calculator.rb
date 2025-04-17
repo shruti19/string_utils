04a9b0b741e9500ef904410735df0474ea8f8da0
shrutis <shruti.s.2702@gmail.com>
2025-04-17 18:39:42 +0530

    added regex for multichar delimiters and validation of strings with multichar delims

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,10 @@
 class StringCalculator
-  NEW_DELIM_PREFIX_REGEX = /^\/\/(.)\n/
-  VALID_STRING_REGEX = lambda {|new_delim| /\A( ?\d+ ?([\n,#{new_delim}] ?\d+)*)*\z/}
+  SINGLECHAR_DELIM_REGEX = /^\/\/(.*)\n/
+  MULTICHAR_DELIM_REGEX = /\[(.*?)\]/
+  VALID_SINGLE_CHAR_DELIM_STRING = lambda {|new_delim| /\A( ?\d+ ?([\n,#{new_delim}] ?\d+)*)*\z/}
+  VALID_MIXED_CHAR_DELIM_STRING = lambda {|singlechar_delim, multichar_delim|
+                      /\A(\d+(?:[,\n#{singlechar_delim}]|([#{multichar_delim}])\2+)?)*\d*\z/ 
+                  }
 
   def self.add str
     sum = 0
@@ -10,7 +14,7 @@ class StringCalculator
     raise "negatives not allowed (found #{negitives.join(', ')})" if !negitives.empty?
 
     return "Invalid Input" if !valid?(str)
-    nums = str.scan(/\d+/)
+    nums = str.scan(/\d+/).select {|n| n.to_i < 1000}
     nums.each do |s|
       sum += s.to_i
     end
@@ -19,13 +23,32 @@ class StringCalculator
   end
 
   def self.valid? str
-    new_delim = [str].grep(NEW_DELIM_PREFIX_REGEX){$1}[0]
+    singlechar_delims = str.scan(SINGLECHAR_DELIM_REGEX).flatten[0].to_s
+    multi_delims = str.scan(MULTICHAR_DELIM_REGEX).flatten
     
-    if new_delim
-      str.sub!(NEW_DELIM_PREFIX_REGEX, '')
+    if !singlechar_delims.empty?
+      ## Remove new delim prefix
+      str.sub!(SINGLECHAR_DELIM_REGEX, '')
+    end
+    
+    if !multi_delims.empty?
+      prefix = "//[" + multi_delims.join("][") + "]\n"
+      ## Remove new delim prefix
+      str.gsub!(prefix, '')
+
+      ## Append all single char delims
+      singlechar_delims += multi_delims.select{|d| d.length == 1}.join('')
+      multichar_delim = multi_delims.select{|d| d.length > 1}
     end
 
-    return !!(str =~ VALID_STRING_REGEX.call(new_delim))
+    if multichar_delim && !multichar_delim.empty?
+      return !!(str =~ VALID_MIXED_CHAR_DELIM_STRING.call(singlechar_delims, multichar_delim)) 
+    else
+      return !!(str =~ VALID_SINGLE_CHAR_DELIM_STRING.call(singlechar_delims))
+    end
   end
 
 end
+
+
+StringCalculator.add("1,2,3")
